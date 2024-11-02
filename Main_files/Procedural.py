c0a26970e48cedd4b01ea9bac994d6ac3388057f
unknown <sye7qjm3ac@icloud.com>
2024-11-02 23:28:47 +0700

    v2.0 (procedural ver is done)

--- Main_files/Procedural.py
+++ Main_files/Procedural.py
@@ -1,4 +1,6 @@
-from Words_convert import value_list, keys_list, words_json
+from Words_convert import value_list, keys_list
+seed = 13 ** 17
+
 
 chars = ['-'] * 33
 i = -1
@@ -19,7 +21,6 @@ for _ in keys_list:
     len_keys_list += 1
 
 max_value = len_keys_list - 1
-seed = 12 ** 17
 # ----------------------------------------------
 was_use = []
 
@@ -36,6 +37,7 @@ while answer in 'yY':
     # Оформление
     print('\n' * 50)
     print('-------------------------------------------------')
+    print('(для выхода из игры введите EXIT)')
     print('КОМПЬЮТЕР ЗАГАДАЛ СЛОВО:')
 
     # Выбираем случайное число не выбранное ранее
@@ -62,20 +64,58 @@ while answer in 'yY':
         j += 1
         if j == random_num_result:
             value_select = value
-
     # Помещаем выбранное слово в массив по буквам
     len_key_select = 0
     for char in key_select:
         len_key_select += 1
-
+    print(value_select)
     word = [None] * len_key_select
+    mask = ['▒▒'] * len_key_select
     i = -1
     for char in key_select:
         i += 1
         word[i] = char
 
+    # Цикл МАСКА -> ПРОМТ БУКВЫ пока все не откроются
+    guessed_letters = True
+    guess_letter = None
+    while guessed_letters:
+        for element in mask:
+            print(element, end=' ')
+        print('\n>>> ',end='')
+        guess_letter = input()
+        if guess_letter == 'EXIT':
+            break
+
+        mask_new = []
+        i = -1
+        for letter in word:
+            i += 1
+            if letter == guess_letter:
+                mask_new += [guess_letter]
+                guessed_letters += 1
+            else:
+                j = -1
+                for letter_mask in mask:
+                    j += 1
+                    if j == i and letter_mask != '▒▒':
+                        mask_new += [letter_mask]
+                        guessed_letters += 1
+                    elif j == i: mask_new += ['▒▒']
+        mask = mask_new
+        flag = False
+        for letter in mask:
+            if letter == '▒▒':
+                flag = True
+        if not flag:
+            guessed_letters = False
+            for element in mask:
+                print(element, end=' ')
+
     # ДЕКОР + ПРОМТ
     print('\n')
+    if not guessed_letters:
+        print('ПОЗДРАВЛЯЕМ С ПОБЕДОЙ!')
     print('-------------------------------------------------')
     print('Загадываем слово дальше? (y)')
     print('>>> ', end='')
